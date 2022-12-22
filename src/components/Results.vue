359c9e17955be4d08da00a301bab0f9092401ad0
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2022-12-22 12:15:32 +0100

    created the block and results components and finished developing the game

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h4>Your reaction time was</h4>
+    <p>{{ score }}ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja fingers';
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes';
+    } else {
+      this.ranl = 'Slow poke';
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
